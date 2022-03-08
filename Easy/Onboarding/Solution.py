167d8a5baf7f9fdd4ea12ad40f6d1bac1c1dc4ef
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-03-08 13:32:14 +0000

    Add files via upload

---
+++ Easy/Onboarding/Solution.py
@@ -0,0 +1,12 @@
+while 1: # game loop
+
+    # VARIABLES
+    enemy_1 = input()  # name of enemy 1
+    enemy_2 = input()  # name of enemy 2
+    dist_1 = int(input())  # distance to enemy 1
+    dist_2 = int(input())  # distance to enemy 2
+
+    if dist_1 < dist_2:
+        print(enemy_1)
+    else:
+        print(enemy_2)
