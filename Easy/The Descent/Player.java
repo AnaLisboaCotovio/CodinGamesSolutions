9f36218fa57fd17e644b07b26d90941663eda213
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-03-22 16:56:44 +0000

    Update Player.java

--- Easy/The Descent/Player.java
+++ Easy/The Descent/Player.java
@@ -8,7 +8,7 @@ class Player {
         while (true) { // Game loop - Loops everytime the ship changes direction
             int max = 0; // The max height of a mountain
             int idmax = 0; // The id of the mountain with the maximum height
-            for (int i = 0; i < 8; i++) { // Looping within the 8 - the amount of mountains everytime the 
+            for (int i = 0; i < 8; i++) { // Looping within the 8 - the amount of mountains
                 int mountainH = in.nextInt(); // Represents the height of one mountain just given as input
             
                 if(mountainH > max){ // If the input mountain's height is bigger than the maximum
