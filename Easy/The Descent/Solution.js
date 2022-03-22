128297226c6a76249497cd086285aede03de1f57
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-03-22 17:42:44 +0000

    Update Solution.js

--- Easy/The Descent/Solution.js
+++ Easy/The Descent/Solution.js
@@ -1,6 +1,7 @@
 while (true) { // Game loop - Loops everytime the ship changes direction
     let max = 0; // The max height of a mountain
     let idmax = 0; // The id of the mountain with the maximum height
+    
     for (let i = 0; i < 8; i++) { // Looping within the 8 - the amount of mountains
         const mountainH = parseInt(readline()); // Represents the height of one mountain just given as input
 
