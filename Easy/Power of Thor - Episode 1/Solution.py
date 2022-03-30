11e4b81753948c8977af6e99cce249a87ef69eac
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-03-30 15:11:40 +0100

    Update Solution.py

--- Easy/Power of Thor - Episode 1/Solution.py
+++ Easy/Power of Thor - Episode 1/Solution.py
@@ -1,46 +1,44 @@
-import sys
-import math
+# lightX: The X position of the light of power
+# lightY: The Y position of the light of power
+# thorX: Thor's starting X position
+# thorY: Thor's starting Y position
+lightX, lightY, thorX, thorY = [int(i) for i in input().split()]
 
-light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
+while True: # Game loop
+    direction = "" # Direction in which thor will move in each turn
 
-# VARIABLES
-thorX = initial_tx # Thor's current X position
-thorY = initial_ty # Thor's current Y position
-direction = "" # The direction Thor will be taking this turn
-
-while True: # game loop
-    remaining_turns = int(input())  # The remaining amount of turns Thor can move.
-
-    if light_x > thorX and light_y < thorY:
+    # Compare Thor's coordinates with the light of power's
+    # Give a direction for thor to move
+    # Change Thor's coordinates accordingly
+    if lightX > thorX and lightY < thorY:
         direction = "NE"
         thorX += 1
         thorY -= 1
-    elif light_x < thorX and light_y < thorY:
+    elif lightX < thorX and lightY < thorY:
         direction = "NW"
         thorX -= 1
         thorY -= 1
-    elif light_x < thorX and light_y > thorY:
+    elif lightX < thorX and lightY > thorY:
         direction = "SW"
         thorX -= 1
         thorY += 1
-    elif light_x > thorX and light_y > thorY:
+    elif lightX > thorX and lightY > thorY:
         direction = "SE"
         thorX += 1
         thorY += 1
     else:
-        if light_x > thorX:
+        if lightX > thorX:
             direction = "E"
             thorX += 1
-        elif light_x < thorX:
+        elif lightX < thorX:
             direction = "W"
             thorX -= 1
         
-        if light_y > thorY:
+        if lightY > thorY:
             direction = "S"
             thorY += 1
-        elif light_y < thorY:
+        elif lightY < thorY:
             direction = "N"
             thorY -= 1
 
-
-    print(direction) # the final direction Thor takes this turn
+    print(direction) # Output the direction in which Thor will move this turn
