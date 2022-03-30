0c0d3af07af344286b6b5e7fde75f187715c6761
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-03-30 15:51:36 +0100

    Update Solution.py

--- Easy/Temperatures/Solution.py
+++ Easy/Temperatures/Solution.py
@@ -1,26 +1,22 @@
-import sys
-import math
-
-# VARIABLES
 n = int(input())  # The number of temperatures to analyse
-closest = float('inf')  # Bigger number than all others so that is can be shortened
+closest = float('inf')  # The closest value to 0 initialized as the max value so it can be shortened 
 signt = None # Will store TRUE if positive and FALSE if negative
 
-if n <= 0 :
-    closest = 0
+if n <= 0 : # If there are no temperatures
+    closest = 0 # Set 0 as the closest temperature (requested in the rules)
 
-for i in input().split() :
-    t = int(i) 
+for i in input().split() : # For all the temperatures
+    t = int(i) # The temperature
 
-    if (t > 0) : signt = True
+    # Set the sign of that temperature
+    if (t > 0) : signt = True 
     else: signt = False
 
-    if (abs(t) < abs(closest)) : # If the positive value of the number is  smaller the closest one
+    # Set the closes value to the temperature in question in these 2 scenarios:
+    if (abs(t) < abs(closest)) : # If the absolute value of the number is smaller than the absolute value of the closest one
         closest = t
-    elif (t == abs(closest) and signt == True) : # If the positive value of the number is the same as the closest one and the t is positive
-        closest = t
-    elif (t == closest and signt == False) : # If the value of the number is the same as the closest one and the t is negative
+    elif (t == abs(closest) and signt == True) : # If the absolute value of the number is the same as the absolute value of the closest one and it is positive
         closest = t
 
 
-print(closest)
+print(closest) # Output the temperature that is closest to 0
