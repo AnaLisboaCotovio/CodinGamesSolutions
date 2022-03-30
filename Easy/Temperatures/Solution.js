95491667f74816c51d426a3c43b856db460be61d
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-03-30 15:44:21 +0100

    Update Solution.js

--- Easy/Temperatures/Solution.js
+++ Easy/Temperatures/Solution.js
@@ -1,30 +1,29 @@
-// VARIABLES
 const n = parseInt(readline()); // The number of temperatures to analyse
 var inputs = readline().split(' ');
-let closest = Number.MAX_SAFE_INTEGER + 1; // Max value so it can be shortened 
+let closest = Number.MAX_SAFE_INTEGER + 1; // The closest value to 0 initialized as the max value so it can be shortened 
 let signt; // Will store TRUE if positive and FALSE if negative
 
-if (n <= 0){
-    closest = 0
+if (n <= 0){ // If there are no temperatures
+    closest = 0 // Set 0 as the closest temperature (requested in the rules)
 }
 
-for (let i = 0; i < n; i++){
-    const t = parseInt(inputs[i]);
+for (let i = 0; i < n; i++){ // For all the temperatures
+    const t = parseInt(inputs[i]); // The temperature
 
-    if(t > 0){ signt = true}
-    else { signt = false}
+    if(t > 0){ // Set the sign of that temperature
+        signt = true
+    } else{ 
+        signt = false
+    }
 
-    if(Math.abs(t) < Math.abs(closest)){ // If the positive value of the number is  smaller the closest one
+    // Set the closes value to the temperature in question in these 2 scenarios:
+    if(Math.abs(t) < Math.abs(closest)){ // If the absolute value of the number is smaller than the absolute value of the closest one
         closest = t;
     } else {
-        if(t == Math.abs(closest) && signt == true){ // If the positive value of the number is the same as the closest one and the t is positive
+        if(t == Math.abs(closest) && signt == true){ // If the absolute value of the number is the same as the absolute value of the closest one and it is positive
             closest = t;
-        } else {
-            if(t == closest && signt == false){ // If the value of the number is the same as the closest one and the t is negative
-                closest = t;
-            }
         }
     }
 }
 
-console.log(closest);
+console.log(closest); // Output the temperature that is closest to 0
