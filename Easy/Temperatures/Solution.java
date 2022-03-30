16adcbe20a7517b0ac8dba366c06d311f9237337
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-03-30 15:39:55 +0100

    Update Solution.java

--- Easy/Temperatures/Solution.java
+++ Easy/Temperatures/Solution.java
@@ -1,40 +1,35 @@
 import java.util.*;
-import java.io.*;
-import java.math.*;
 
 class Solution {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
-        // VARIABLES
-        int n = in.nextInt(); // the number of temperatures to analyse
-        int closest = Integer.MAX_VALUE; // Max value so it can be shortened 
+        int n = in.nextInt(); // The number of temperatures to analyse
+        int closest = Integer.MAX_VALUE; // The closest value to 0 initialized as the max value so it can be shortened 
         Boolean signt; // Will store TRUE if positive and FALSE if negative
 
         if (n <= 0) { // If there are no temperatures
-                closest = 0;
-        }
-        
-        for (int i = 0; i < n; i++) {
-            int t = in.nextInt(); 
+                closest = 0; // Set 0 as the closest temperature (requested in the rules)
+        } else{
+            for (int i = 0; i < n; i++) { // For all the temperatures
+                int t = in.nextInt(); // The temperature
+                if(t > 0){ // Set the sign of that temperature
+                    signt = true;
+                } else{
+                    signt = false;
+                }
 
-            if(t > 0){ signt = true;} 
-            else{ signt = false;}
-
-            if (Math.abs(t) < Math.abs(closest)) { // If the positive value of the number is  smaller the closest one
-                closest = t;
-            } else {
-                if (t == Math.abs(closest) && signt == true) { // If the positive value of the number is the same as the closest one and the t is positive
+                // Set the closes value to the temperature in question in these 2 scenarios:
+                if (Math.abs(t) < Math.abs(closest)) { // If the absolute value of the number is smaller than the absolute value of the closest one
                     closest = t;
-                } else{ 
-                    if(t == closest && signt == false){ // If the value of the number is the same as the closest one and the t is negative
-                        closest = t;
-                    }
+                } else {
+                    if (Math.abs(t) == Math.abs(closest) && signt == true) { // If the absolute value of the number is the same as the absolute value of the closest one and it is positive
+                        closest = t; 
+                    } 
                 }
             }
         }
-        
-        System.out.println(closest);
+        System.out.println(closest); // Output the temperature that is closest to 0
     }
 }
