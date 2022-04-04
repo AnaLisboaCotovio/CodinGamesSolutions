2999c2eb9b2d5531f24f6df176abca94cfb498e1
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-04-04 02:25:21 +0100

    Update Solution.py

--- Easy/Temperatures/Solution.py
+++ Easy/Temperatures/Solution.py
@@ -1,21 +1,19 @@
 n = int(input())  # The number of temperatures to analyse
-closest = float('inf')  # The closest value to 0 initialized as the max value so it can be shortened 
-signt = None # Will store TRUE if positive and FALSE if negative
+closest = float('inf')
+signt = None # TRUE if temperature positive and FALSE if temperature negative
 
-if n <= 0 : # If there are no temperatures
-    closest = 0 # Set 0 as the closest temperature (requested in the rules)
+if n <= 0 : 
+    closest = 0 
 
-for i in input().split() : # For all the temperatures
-    t = int(i) # The temperature
+for i in input().split() : 
+    t = int(i) # The temperatures
 
-    # Set the sign of that temperature
     if (t > 0) : signt = True 
     else: signt = False
 
-    # Set the closes value to the temperature in question in these 2 scenarios:
-    if (abs(t) < abs(closest)) : # If the absolute value of the number is smaller than the absolute value of the closest one
+    if (abs(t) < abs(closest)) : 
         closest = t
-    elif (t == abs(closest) and signt == True) : # If the absolute value of the number is the same as the absolute value of the closest one and it is positive
+    elif (t == abs(closest) and signt == True) :
         closest = t
 
-print(closest) # Output the temperature that is closest to 0
+print(closest)
