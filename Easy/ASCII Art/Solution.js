56a9c9a650b5b85df0e68101261d085afdba3ce5
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-04-04 01:40:29 +0100

    Update Solution.js

--- Easy/ASCII Art/Solution.js
+++ Easy/ASCII Art/Solution.js
@@ -1,40 +1,40 @@
-const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; // A string that contains all the letters of the alphabet in english
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
 
 const l = parseInt(readline()); // The length of each letter
 const h = parseInt(readline()); // The height of each letter
 const t = readline(); // The word to print in ASCII art
-const word = t.toUpperCase(); // The word in uppercase
-var ids = getIds(word); // Get the id's of each letter in the alphabet's order
+const word = t.toUpperCase(); 
+var ids = getIds(word); // Stores the id's of each letter in the alphabet
 
-for (let i = 0; i < h; i++) { // Loop for the height of the letters
-    const az = readline(); // Input of the string of each height of all the letter's ASCII art in alphabetical order
-    console.log(getArt(ids, l, az)); // Get the art of this height of the letter's of the word in order
+for (let i = 0; i < h; i++) {
+    const az = readline(); // The ASCII Art string
+    console.log(getArt(ids, l, az));
 }
 
 function getIds(w){
-    let ids = []; // An array for the id's 
-    let s = ""; // This string will contain each letter of the word
+    let ids = [];
+    let s = "";
 
-    for(let i = 0; i < w.length; i++){ // Loop for the length of the word
+    for(let i = 0; i < w.length; i++){ 
         s = w.charAt(i); // Get each letter of the word
 
-        if(alphabet.includes(s)){ // If that letter is in the alphabet 
-            ids[i] = alphabet.indexOf(s); // Get the id of that letter in the alphabet
-        } else { // If the letter is not part of the alphabet
-            ids[i] = alphabet.length; // Get the length id of the alphabet
+        if(alphabet.includes(s)){ 
+            ids[i] = alphabet.indexOf(s); 
+        } else {
+            ids[i] = alphabet.length; 
         }
     }
-    return ids; // Return the list of id's
+    return ids; 
 }
 
 function getArt(ids, l, az){
-    let art = ""; // String that will have the ASCII art of the letters of the word
+    let art = ""; 
 
-    for(let i = 0; i < ids.length; i++){ // Loop over the list of id's
-        let index1 = ids[i] * l; // Will store the first index of each letter
-        let index2 = index1 + l; // Will store the last indes of each letter
+    for(let i = 0; i < ids.length; i++){ 
+        let index1 = ids[i] * l; // Stores first index of each letter
+        let index2 = index1 + l; // Stores last index of each letter
 
-        art += az.slice(index1, index2); // Add the string of the letter in question
+        art += az.slice(index1, index2); 
     }
-    return art; // Return the ASCII art of this height of the word
+    return art;
 }
