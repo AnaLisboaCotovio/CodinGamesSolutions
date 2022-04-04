c292cae7bb688340ee21557c04f37361151db3a1
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-04-04 01:50:46 +0100

    Update Player.java

--- Easy/Power of Thor - Episode 1/Player.java
+++ Easy/Power of Thor - Episode 1/Player.java
@@ -1,55 +1,63 @@
-var inputs = readline().split(' ');
-const lightX = parseInt(inputs[0]); 
-const lightY = parseInt(inputs[1]);
-let thorX = parseInt(inputs[2]); 
-let thorY = parseInt(inputs[3]); 
+import java.util.*;
 
-while (true) {
-    let direction = ""; 
-    
-    if(lightX > thorX && lightY < thorY){
-        direction = "NE";
-        thorX++;
-        thorY--;
-    }else{
-        if(lightX < thorX && lightY < thorY){
-            direction = "NW";
-            thorX--;
-            thorY--;
-        }else{
-            if(lightX < thorX && lightY > thorY){
-                direction = "SW";
-                thorX--;
-                thorY++;
+class Player {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+
+        int lightX = in.nextInt(); 
+        int lightY = in.nextInt(); 
+        int thorX = in.nextInt(); 
+        int thorY = in.nextInt(); 
+
+        while (true) { 
+            String direction = ""; 
+
+            if(lightX > thorX && lightY < thorY){
+                direction = "NE";
+                thorX++;
+                thorY--;
             }else{
-                if(lightX > thorX && lightY > thorY){
-                    direction = "SE";
-                    thorX++;
-                    thorY++;
+                if(lightX < thorX && lightY < thorY){
+                    direction = "NW";
+                    thorX--;
+                    thorY--;
                 }else{
-                    if(lightX > thorX){
-                        direction = "E";
-                        thorX++;
-                    }else{
-                        if(lightX < thorX){
-                            direction = "W";
-                            thorX--;
-                        }
-                    }
-
-                    if(lightY > thorY){
-                        direction = "S";
+                    if(lightX < thorX && lightY > thorY){
+                        direction = "SW";
+                        thorX--;
                         thorY++;
                     }else{
-                        if(lightY < thorY){
-                           direction = "N";
-                            thorY--; 
+                        if(lightX > thorX && lightY > thorY){
+                            direction = "SE";
+                            thorX++;
+                            thorY++;
+                        }else{
+                            if(lightX > thorX){
+                                direction = "E";
+                                thorX++;
+                            }else{
+                                if(lightX < thorX){
+                                    direction = "W";
+                                    thorX--;
+                                }
+                            }
+
+                            if(lightY > thorY){
+                                direction = "S";
+                                thorY++;
+                            }else{
+                                if(lightY < thorY){
+                                   direction = "N";
+                                    thorY--; 
+                                }
+                            }
                         }
                     }
                 }
             }
+
+            System.out.println(direction);
         }
     }
-
-    console.log(direction); 
 }
