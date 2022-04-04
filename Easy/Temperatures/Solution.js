eb3936c27bd829252f13f4784a68a1deea8a7e81
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-04-04 02:25:04 +0100

    Update Solution.js

--- Easy/Temperatures/Solution.js
+++ Easy/Temperatures/Solution.js
@@ -1,29 +1,28 @@
 const n = parseInt(readline()); // The number of temperatures to analyse
 var inputs = readline().split(' ');
-let closest = Number.MAX_SAFE_INTEGER + 1; // The closest value to 0 initialized as the max value so it can be shortened 
-let signt; // Will store TRUE if positive and FALSE if negative
+let closest = Number.MAX_SAFE_INTEGER + 1; 
+let signt; // TRUE if temperature positive and FALSE if temperature negative
 
-if (n <= 0){ // If there are no temperatures
-    closest = 0 // Set 0 as the closest temperature (requested in the rules)
+if (n <= 0){ 
+    closest = 0 
 }
 
-for (let i = 0; i < n; i++){ // For all the temperatures
-    const t = parseInt(inputs[i]); // The temperature
+for (let i = 0; i < n; i++){ 
+    const t = parseInt(inputs[i]); // The temperatures
 
-    if(t > 0){ // Set the sign of that temperature
+    if(t > 0){ 
         signt = true
     } else{ 
         signt = false
     }
 
-    // Set the closes value to the temperature in question in these 2 scenarios:
-    if(Math.abs(t) < Math.abs(closest)){ // If the absolute value of the number is smaller than the absolute value of the closest one
+    if(Math.abs(t) < Math.abs(closest)){ 
         closest = t;
     } else {
-        if(t == Math.abs(closest) && signt == true){ // If the absolute value of the number is the same as the absolute value of the closest one and it is positive
+        if(t == Math.abs(closest) && signt == true){ 
             closest = t;
         }
     }
 }
 
-console.log(closest); // Output the temperature that is closest to 0
+console.log(closest);
