f26f0b61d17a2565b5b7132a4dedc5555572864a
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2022-04-04 01:46:57 +0100

    Update Solution.py

--- Easy/ASCII Art/Solution.py
+++ Easy/ASCII Art/Solution.py
@@ -1,34 +1,33 @@
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # A string that contains all the letters of the alphabet in english
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
 
 def getIds (w):
-    ids = [] # An array for the id's 
+    ids = [] 
 
-    for i in range(len(w)): # Loop for the length of the word
-        # For each letter in the word
-        if(w[i] in alphabet): # If that letter is in the alphabet 
-            ids.append(alphabet.index(w[i])) # Get the id of that letter in the alphabet
-        else: # If the letter is not part of the alphabet
-            ids.append(len(alphabet)) # Get the length id of the alphabet
+    for i in range(len(w)):
+        if(w[i] in alphabet): # Apply to each letter of the word
+            ids.append(alphabet.index(w[i])) 
+        else: 
+            ids.append(len(alphabet)) 
         
-    return ids # Return the list of id's
+    return ids 
 
 def getArt (ids, l, az):
-    art = "" # String that will have the ASCII art of the letters of the word
+    art = "" 
 
-    for i in range(len(ids)): # Loop over the list of id's
-        index1 = ids[i] * l # Will store the first index of each letter
-        index2 = index1 + l # Will store the last indes of each letter
+    for i in range(len(ids)): 
+        index1 = ids[i] * l # Stores first index of each letter
+        index2 = index1 + l # Stores last index of each letter
 
-        art += az[index1:index2] # Add the string of the letter in question
+        art += az[index1:index2]
     
-    return art # Return the ASCII art of this height of the word
+    return art 
 
 l = int(input()) # The length of each letter
 h = int(input()) # The height of each letter
-t = input() # The word to print in ASCII art
-word = t.upper() # The word in uppercase
-ids = getIds(word) # Get the id's of each letter in the alphabet's order
+t = input() # The word requested in ASCII art
+word = t.upper()
+ids = getIds(word) # Stores the id's of each letter in the alphabet
 
-for i in range(h): # Loop for the height of the letters
-    az = input() # Input of the string of each height of all the letter's ASCII art in alphabetical order
-    print(getArt(ids, l, az)) # Get the art of this height of the letter's of the word in order
+for i in range(h): 
+    az = input() # The ASCII Art string
+    print(getArt(ids, l, az)) 
