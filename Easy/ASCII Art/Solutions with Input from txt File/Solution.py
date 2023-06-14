7910a9408c328f8b087a1593cf5942ea750f4021
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2023-06-14 18:14:39 +0200

    Add files via upload

---
+++ Easy/ASCII Art/Solutions with Input from txt File/Solution.py
@@ -0,0 +1,41 @@
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" 
+
+with open("E:/Github Projects/CodinGames - Practices/Easy/ASCII Art/Test Sets/test6.txt") as file: # Paste Source path of the txt file of the input
+    lines = file.readlines()
+line = [x[:-1] for x in lines]
+
+def getIds (w):
+    ids = [] 
+
+    for i in range(len(w)):
+        if(w[i] in alphabet): # Apply to each letter of the word
+            ids.append(alphabet.index(w[i])) 
+        else: 
+            ids.append(len(alphabet)) 
+        
+    return ids 
+
+def getArt (ids, l, az):
+    art = "" 
+
+    for i in range(len(ids)): 
+        index1 = ids[i] * l # Stores first index of each letter
+        index2 = index1 + l # Stores last index of each letter
+
+        art += az[index1:index2]
+    
+    return art 
+
+l = int(line[0]) # The length of each letter
+h = int(line[1]) # The height of each letter
+t = str(line[2]) # The word requested in ASCII art
+word = t.upper()
+ids = getIds(word) # Stores the id's of each letter in the alphabet
+
+print("\n")
+print("String we want to transform: ", t)
+print("String transformmed:")
+
+for i in range(h): 
+    az = str(line[3+i]) # The ASCII Art string
+    print(getArt(ids, l, az)) 
