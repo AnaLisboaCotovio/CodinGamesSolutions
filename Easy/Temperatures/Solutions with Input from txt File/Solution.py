d1ea0d829432f28644f4200129b80e1eb2717fc7
Ana Lisboa Cotovio <64485515+AnaLisboaCotovio@users.noreply.github.com>
2023-06-17 12:32:45 +0200

    Add files via upload

---
+++ Easy/Temperatures/Solutions with Input from txt File/Solution.py
@@ -0,0 +1,35 @@
+with open("Test Sets/test6.txt") as  file: # Paste Source path of the txt file of the input
+    lines = file.readlines()
+line = [x[:-1] for x in lines[:-1]]
+line.append(lines[-1])
+
+n = int(line[0])  # The number of temperatures to analyse
+closest = float('inf')
+signt = None # TRUE if temperature positive and FALSE if temperature negative
+
+print()
+print("From the  following temperatures: ", end = " ")
+
+#if n <= 0 : 
+#    closest = 0 
+#    print("0")
+
+if int(line[0]) == 0 :
+    print("0", end = " ")
+    closest = 0
+else:
+    for i in line[1].split() : 
+        t = int(i) # The temperatures
+        print(t, end = "  ")
+
+        if (t > 0) : signt = True 
+        else: signt = False
+
+        if (abs(t) < abs(closest)) : 
+            closest = t
+        elif (t == abs(closest) and signt == True) :
+            closest = t
+
+print()
+print("The one that is closest to zero is: ", closest)
+print()
